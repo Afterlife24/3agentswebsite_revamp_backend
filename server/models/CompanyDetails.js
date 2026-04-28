02338a724490eb90a849181645f561ad1e4762a8
Dhanush6371 <dhanushvardhan6371@gmail.com>
2026-04-28 08:04:09 +0530

    Make userId optional in CompanyDetails model

--- server/models/CompanyDetails.js
+++ server/models/CompanyDetails.js
@@ -4,8 +4,8 @@ const companyDetailsSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true,
-        unique: true
+        required: false, // Changed to optional since users don't need to be logged in
+        unique: false // Changed to allow multiple entries without userId
     },
     companyName: {
         type: String,
